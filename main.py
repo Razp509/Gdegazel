29d6f63b021a10626483205079befabf50c89aff
Razp509 <Razp509@gmail.com>
2024-05-11 15:53:48 +0500

    stable 0.1

--- main.py
+++ main.py
@@ -12,20 +12,17 @@ dt = datetime.now()
 month = dt.month
 day = dt.day
 
-# apply format
-for i in range(1, days_in_month(month)):
-    col = f'A{i}'
+
+lst_col = {4:'A', 5:'B', 6:'C', 7:'D', 8:'E', 9:'F', 10:'G', 11:'H', 12:'I'}
+
+num_col = lst_col.get(month)
+
+
+for i in range(4, days_in_month(month)):
+    col = f'{num_col}{i}'
     header = wks.cell(col)
     if header.value == '':
         wks.apply_format([col], ["NUMBER"])
         header.value = day
         header.update()
         break
-
-
-
-
-## set a formula and extend it to more cells
-#cell = wks.cell('C1')
-#cell.value = '=A1+B1'
-#wks.apply_format('C1:C10', cell, 'userEnteredValue.formulaValue')
