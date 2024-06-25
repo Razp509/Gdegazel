95a3a605828dd337d8c7ebfe5d8a17fd98128d7e
Razp509 <Razp509@gmail.com>
2024-06-26 00:01:11 +0500

    stable 2.0

--- main.py
+++ main.py
@@ -7,20 +7,25 @@ gc = pygsheets.authorize(service_file='gdegazel-service-acount.json')
 sh = gc.open('Gdegazel_table')
 wks = sh.sheet1
 
-dt = datetime.now()
-month = dt.month
-day = dt.day
-
 
 col_month = {4:'B', 5:'D', 6:'F', 7:'H', 8:'J', 9:'L', 10:'N', 11:'P', 12:'R'}
-col_month_value = col_month.get(month)
 work_day_month = {4:'A', 5:'C', 6:'E', 7:'G', 8:'I', 9:'K', 10:'M', 11:'O', 12:'Q' }
-work_day_month_value = work_day_month.get(month)
 
+
+def month():
+    dt = datetime.now()
+    month = dt.month
+    return month
+
+
+def day():
+    dt = datetime.now()
+    day = dt.day
+    return day
 
         
 def into_cell(money_received):
-    col = f'{col_month_value}{day + 1}'
+    col = f'{col_month.get(month())}{day() + 1}'
     value_cell = wks.cell(col)    
     if value_cell.value == '':
         value_cell.value = money_received
@@ -31,7 +36,7 @@ def into_cell(money_received):
     
 
 def cell_clear():
-    col = f'{col_month_value}{day + 1}'
+    col = f'{col_month.get(month())}{day() + 1}'
     value_cell = wks.cell(col)
     # value_cell.value = ''
     value_cell.value = ''
@@ -47,7 +52,7 @@ def all_work_day():
 
 
 def work_day():
-    col = f'{work_day_month_value}45'
+    col = f'{work_day_month.get(month())}45'
     cell_col = wks.cell(col)    
     val = int(cell_col.value)
     return val
@@ -61,7 +66,7 @@ def all_work_money():
 
 
 def work_money():
-    col = f'{col_month_value}45'
+    col = f'{col_month.get(month())}45'
     cell_col = wks.cell(col)
     val = int(cell_col.value)
     return val
